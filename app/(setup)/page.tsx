2c434957c7ebfe3b135314837500e14200406c43
hazel <hazel_faye@outlook.com>
2024-08-28 01:07:24 +0800

    deleted:    app/(main)/(routers)/page.tsx 	new file:   app/(setup)/page.tsx 	new file:   app/api/uploadthing/core.ts 	new file:   app/api/uploadthing/route.ts 	modified:   app/layout.tsx 	new file:   components/modals/initial-modal.tsx 	modified:   components/ui/button.tsx 	new file:   components/ui/dialog.tsx 	new file:   components/ui/form.tsx 	new file:   components/ui/input.tsx 	new file:   components/ui/label.tsx 	new file:   lib/db.ts 	new file:   lib/initial-profile.ts 	new file:   lib/uploadthing.ts 	modified:   middleware.ts 	modified:   package.json 	modified:   pnpm-lock.yaml 	new file:   prisma/schema.prisma

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,27 @@
+import { initialProfile } from "@/lib/initial-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import { InitialModal } from "@/components/modals/initial-modal";
+
+const SetupPage = async () => {
+    const profile = await initialProfile();
+
+    //search every servers, find the first server has the profile id
+    const server = await db.server.findFirst({
+        where: {
+            members: {
+                some: {
+                    profileId: profile.id
+                }
+            }
+        }
+    });
+
+    if (server) {
+        return redirect(`/servers/{server.id}`);
+    }
+
+    return <InitialModal />
+}
+
+export default SetupPage;
--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware, clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
+
 
 export const config = {
   matcher: [
